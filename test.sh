85594e716fe38c256a37026595b4efad7e993341
Chab <chabane.hamma@gmail.com>
2023-10-31 14:15:16 +0100

    add tests

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 mvn clean test -P chrome
 if [ $? -ne 0 ]; then
-  echo "Test failed" >> test-status.txt
+  echo "Test failed" >> $GITHUB_OUTPUT
 else
-  echo "Test passed" >> test-status.txt
+  echo "Test passed" >> $GITHUB_OUTPUT
 fi
